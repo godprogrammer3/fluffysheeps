67e6ff514b27310e28eafcf7e212d6bfc6f06f36
godprogrammer3 <61010103@kmitl.ac.th>
2021-11-08 00:46:51 +0700

    success run empty test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,4 @@
 var Fluffysheeps = artifacts.require('Fluffysheeps');
-module.exports = function(deployer) {
+module.exports = function(deployer, network, addresses) {
     deployer.deploy(Fluffysheeps,process.env.TOKEN_BASE_URI,process.env.CONTRACT_URI);
-    // Additional contracts can be deployed here
 };
